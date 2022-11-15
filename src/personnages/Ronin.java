a3867c7a2d030c065728ef68c61f05c000e6259b
Sydney Lauret <sydney.lauret@gmail.com>
2022-11-15 17:10:13 +0100

    TP5 les humains ont une mémoire

--- src/personnages/Ronin.java
+++ src/personnages/Ronin.java
@@ -20,13 +20,13 @@ public class Ronin extends Humain {
 		if (force >= adversaire.getReputation()) {
 			parler("Je t'ai eu petit yakuza!");
 			int gain = adversaire.perdre();
-			argent += gain;
+			gagnerArgent(gain);
 			honneur++;
 		} else {
 			parler("J'ai perdu contre ce yakuza, mon honneur et ma bourse en ont pris un coup.");
 			int perte = argent;
+			perdreArgent(perte);
 			adversaire.gagner(perte);
-			argent -= perte;
 			if (honneur > 1) {
 				honneur--;
 			}
