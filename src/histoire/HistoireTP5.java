8e3c10fc23cb3dd8beb1cfcc024ebd6c5b001351
Sydney Lauret <sydney.lauret@gmail.com>
2022-11-15 12:14:59 +0100

    TP5 les samouraïs

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -13,6 +13,7 @@ public class HistoireTP5 {
 		Commercant kumi = new Commercant("Kumi", 10);
 		Yakuza yaku = new Yakuza("Yaku Le Noir", "whisky", 30, "Warsong");
 		Ronin roro = new Ronin("Roro", "shochu", 60);
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
 
 		// Scénario
 
@@ -23,5 +24,11 @@ public class HistoireTP5 {
 		marco.listerConnaissance();
 		roro.listerConnaissance();
 		yaku.listerConnaissance();
+
+		System.out.println();
+
+		akimoto.faireConnaissanceAvec(marco);
+		akimoto.listerConnaissance();
+		akimoto.boire("thé");
 	}
 }
